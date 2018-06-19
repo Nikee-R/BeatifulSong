238afa9350c66643a0e15070f85f923d1ad0a5bd
Nikee <rivera.nikee@gmail.com>
2018-06-18 21:32:03 -0400

    Added firebase to Javascript and added table for playlist to the html.

--- assets/JS/myJS.js
+++ assets/JS/myJS.js
@@ -1,5 +1,29 @@
 $(document).ready(function() {
 
+    // Firebase to store user-specific playlist information.
+    <script src="https://www.gstatic.com/firebasejs/5.0.4/firebase.js"></script>
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyCASV3iojfjEk-NCtqWP9PbNaHKRfAesKs",
+            authDomain: "beautifulsong-c6435.firebaseapp.com",
+            databaseURL: "https://beautifulsong-c6435.firebaseio.com",
+            projectId: "beautifulsong-c6435",
+            storageBucket: "",
+            messagingSenderId: "976668913602"
+        };
+
+        firebase.initializeApp(config);
+
+        // Variable that recognizes the database.
+        var database = firebase.database();
+
+    // On-click function that changes song.
+
+    // On-click function that changes the page to the next 10 songs in playlist (limited).
+
+    // On-click that pushes the song clicked in playlist to the top, while simultaneously playing it.
+
+
 
 });
 
