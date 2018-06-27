f8ee856d5447bd4ab736a17892e90402147af9d1
Nikee <rivera.nikee@gmail.com>
2018-06-27 14:39:36 -0400

    playlist functions

--- assets/JS/myJS.js
+++ assets/JS/myJS.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-
-    // Firebase to store user-specific playlist information.
-    <script src="https://www.gstatic.com/firebasejs/5.0.4/firebase.js"></script>
+    
+        // ===================================== Firebase ===================================== 
+        <script src="https://www.gstatic.com/firebasejs/5.0.4/firebase.js"></script>
         // Initialize Firebase
         var config = {
             apiKey: "AIzaSyCASV3iojfjEk-NCtqWP9PbNaHKRfAesKs",
@@ -13,19 +13,120 @@ $(document).ready(function() {
         };
 
         firebase.initializeApp(config);
-
+        
         // Variable that recognizes the database.
         var database = firebase.database();
 
-    // On-click function that changes song.
+            var q = "";
+        
+            var videoId = "";
+        
+            var artistName = "";
+        
+            var trackName = "";
+        
+            //create play-list
+        
+            //search function will take whatever the user typy in 
+            $("#searchButton").on("click", function(){
+                //prevents the submit button from trying to submit a form when clicked
+                event.preventDefault();
+                //a var to hold what user type in
+                var q = $("#userInput").val().trim().toLowerCase();
+        
+                //if user do not type in any word, then show the alert
+                if ( q == "") {
+                  alert("you did not type any word in!")
+                } else {
+                      //search the song of userInput
+                      userSearch = userInput ;}
+        
+                  //youTuBe API key
+                var youTuBeApiKey = "AIzaSyCJ4ygjtnrG-r3EoZ3jiR5ssYiFN_JahCQ" ;
+        
+                 // Constructing a queryURL using the API
+                var queryURL = 	"https://www.googleapis.com/youtube/v3/search?" +
+                                "&part=snippet" +
+                                "&type=video" +
+                                "&q=" + q +
+                                "&maxResults=5" +
+                                "&videoCategoryId=10" +
+                                "&key=" + youTuBeApiKey;
+                    
+                // ===================================== AJAX call ===================================== 
+                $.ajax({
+                  url: queryURL,
+                  method: "GET"
+                }).then (function (response){
+                    console.log(response);
+                    videoId = response.items[0].id.videoId
+                    var ur l= "https://www.youtube.com/embed/" + videoId;
+                    console.log(url);	
+        
+                // ===================================== Changes the song based on user-input to search. ===================================== 
+                videojs('preview-player').ready(function() {
+                    var myPlayer = this;
+                    myPlayer.src({ type: 'video/youtube', src: url });
+                    });
+                    
+                    var str = response.items[0].snippet.title;
+                    var res = str.split("-");
+        
+                    console.log(str);
+                    console.log(res);
+        
+                    artistName = res[0];
+                    trackName = res[1];
+        
+                    console.log(artistName);
+                    console.log(trackName);
+        
+                }); 							
+        
+            });		
+        
+        });	
+        // ===================================== Playlist Functions ===================================== 
+        function initialPlaylist() {
+            var playlistArtist = $('#artist-name').val('');
+            var playlistAlbum = $('#album-name').val('');
+            var playlistSong = $('#song-name').val('');
+         }
 
-    // On-click function that changes the page to the next 10 songs in playlist (limited).
+        // Variable containing an empty array for the playlist.
+        var playlist = [];
 
-    // On-click that pushes the song clicked in playlist to the top, while simultaneously playing it.
+        // Adds songs to playlist  
+        function newSong() {
+            for (i = 0; i < playlist.length; i++) {
+                $('.table > tbody').append(
+                    '<tr>' +
+                        '<td>' + playlist [i].playlistArtist + '</td>' +
+                        '<td>' + playlist [i].playlistAlbum + '<td>' +
+                        '<td>' + playlist [i].playlistSong + '</td>' +
+                    '</tr>'
+                );
+            }
+        }
+        
+        // Toggles play and pause when song is clicked 
+        var audio = document.getElementById('song');
+        var active = false;
 
+        function toggle() {
+            if (active) {
+                audio.pause()
+                        } else {
+                audio.play();
+            }
+        };
 
+        audio.onplaying = function() {
+            active = true;
+        };
 
-});
-
+        audio.onpause = function() {
+            active = false;
+        };
 
 
